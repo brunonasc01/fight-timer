076055c948eec3ffd940b95296153557042a5e61
Bruno Araujo <brnasc.work@proton.me>
2024-10-26 09:44:32 -0300

    turn into app with electrun

---
+++ main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createMainWindow() {
+    const mainWindow = new BrowserWindow({
+        fullscreen: true,
+        autoHideMenuBar: true,
+        icon: path.join(__dirname, 'assets/icons/wkb_icon.ico'),
+        webPreferences: {            
+            nodeIntegration: true,
+            contextIsolation: false,
+        }
+    });
+
+    mainWindow.loadFile('src/index.html');
+}
+
+app.whenReady().then(createMainWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createMainWindow();
+    }
+});
